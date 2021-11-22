a99f4195fb175161e749d29b678f29a77c5e6372
amerucci <a.merucci@onlineformapro.com>
2021-11-22 14:29:45 +0100

    Gestion des pages et des post template

--- wp-content/themes/my-creative-agency-acs/page.php
+++ wp-content/themes/my-creative-agency-acs/page.php
@@ -15,7 +15,7 @@ include('header.php');
 
 
 <div class="container">
-    <h1><?php the_title(); ?></h1>
+    <h1 class="pagetitleh1"><?php the_title(); ?></h1>
     <div class="content">
     <?php the_content(); ?>
     </div>
--- wp-content/themes/my-creative-agency-acs/single.php
+++ wp-content/themes/my-creative-agency-acs/single.php
@@ -1,21 +1,42 @@
-<?php get_header(); ?>
-<div class="main single">
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<div class="post">
-<h1 class="post-title"><?php the_title(); ?></h1>
-<p class="post-info">
-Posté le <?php the_date(); ?> dans <?php the_category(', '); ?> par <?php the_author(); ?>.
-</p>
-<div class="post-content">
-<?php the_content(); ?>
+<?php
+$image =  get_field('images');
+
+?>
+
+<style>
+    .imgaccroche {
+        background: url("<?php echo $image[0]['image_projet']; ?>")
+    }
+</style>
+<?php
+
+include("header.php");
+
+?>
+
+
+<div class="imgaccroche">
+    <h1><?php the_title(); ?></h1>
 </div>
-<div class="post-comments">
+
+<div class="container">
+
+    <div class="contenu">
+        <?php the_content(); ?>
+    </div>
+    <div class="extra">
+        <?php
+        foreach ($image as $the_image) {
+            echo "<img class='img50' src='" . $the_image['image_projet'] . "'/>";
+        }
+        ?>
+    </div>
+    <div class="post-comments">
 <?php comments_template(); ?>
 </div>
 </div>
-<?php endwhile; ?>
-<?php endif; ?>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>c
+
+<?php
+
+
+include("footer.php");
