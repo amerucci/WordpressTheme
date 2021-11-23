1684326f58f234ec6fd35b88af9205c96e163b9b
amerucci <a.merucci@onlineformapro.com>
2021-11-23 09:43:24 +0100

    Mise en forme section contact

--- wp-content/themes/my-creative-agency-acs/template_contact.php
+++ wp-content/themes/my-creative-agency-acs/template_contact.php
@@ -5,6 +5,31 @@
 include('header.php');
 ?>
 
+<style>
+    .navbar{
+        background: #222;
+    }
+</style>
+
+
+<div class="container container-contact">
+<h1 class="pagetitleh1"><?php the_title(); ?></h1>
+<div class="contact-accroche">
+<?php the_content(); ?>
+</div>
+
+
+<div class="col50">
+
+<?php echo the_field('carte'); ?>
+</div>
+<div class="col50">
+    <?php echo get_field('formulaire'); ?>
+</div>
+
+
+</div>
+
 
 
 
